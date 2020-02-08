47261acfd2445628f4b5cbcb34a4d1be25604d1a
Michael Thoreau <mjt9978@nyu.edu>
2020-02-08 11:37:55 -0800

    committing everything in my new branch

--- test.py
+++ test.py
@@ -1,6 +1,6 @@
 import time
-print("you can code in nano")
+print("you can code in nano, in a new branch")
 for i in range(69):
 	print(".")
 	time.sleep(0.1)
-print("but it sucks")
+print("it still sucks")
